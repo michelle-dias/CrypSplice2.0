b31e2b19ef5c66accefbac3670046328b0c03b38
Michelle <michelledias10@gmail.com>
2025-06-20 20:26:16 -0500

    changed main folder name from crypsplice to Crypsplice

--- crypsplice/lib/visualization_imports/pyGenomeTracks/build/lib/pygenometracks/__init__.py
+++
@@ -1,5 +0,0 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.getLogger('matplotlib').setLevel(logging.WARNING)
-logging.getLogger('hicmatrix').setLevel(logging.WARNING)
-logging.getLogger('numexpr').setLevel(logging.WARNING)
